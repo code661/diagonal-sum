b0aef3e900fc1e9ceb89bae2d1671484d61f100d
Tan DuZhi <lucky.tanduzhi@gmail.com>
2019-12-01 01:09:20 +0800

    把傻傻的代码改掉

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -19,11 +19,7 @@ public class Main {
     public static int diagonalSum(int[][] matrix) {
         int result = 0;
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if(i == j ) {
-                    result += matrix[i][j];
-                }
-            }
+            result += matrix[i][i];
         }
         return result;
     }
